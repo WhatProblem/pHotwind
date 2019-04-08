6bcefcf4e5019572d24255ed477e103d4ffe691a
WhatProblem <18355350915@163.com>
2019-04-08 22:01:59 +0800

    文件上传

--- src/admin/Admin.php
+++ src/admin/Admin.php
@@ -150,4 +150,84 @@ class Admin
     $resp = $this->respJson($data);
     return $this->response->withJson($resp);
   }
+
+  /**
+   * Note: 文件上传
+   */
+  public function upload($request, $response)
+  {
+    var_export($_FILES);
+    if ($_FILES['file']['error'] > 0) {
+      switch ($_FILES['file']['error']) {
+         //错误码不为0，即文件上传过程中出现了错误
+        case '1':
+          echo '文件过大';
+          break;
+        case '2':
+          echo '文件超出指定大小';
+          break;
+        case '3':
+          echo '只有部分文件被上传';
+          break;
+        case '4':
+          echo '文件没有被上传';
+          break;
+        case '6':
+          echo '找不到指定文件夹';
+          break;
+        case '7':
+          echo '文件写入失败';
+          break;
+        default:
+          echo "上传出错<br/>";
+      }
+    } else {
+
+      $MAX_FILE_SIZE = 100000;
+      if ($_FILES['file']['size'] > $MAX_FILE_SIZE) {
+        exit("文件超出指定大小");
+
+      }
+
+      $allowSuffix = array(
+        'jpg',
+        'gif',
+      );
+
+      $myImg = explode('.', $_FILES['file']['name']);
+
+      $myImgSuffix = array_pop($myImg);
+
+      if (!in_array($myImgSuffix, $allowSuffix)) {
+        exit("文件后缀名不符");
+      }
+
+      $allowMime = array(
+        "image/jpg",
+        "image/jpeg",
+        "image/pjpeg",
+        "image/gif",
+      );
+
+      if (!in_array($_FILES['file']['type'], $allowMime)) {
+        exit('文件格式不正确，请检查');
+      }
+
+      $path = "public/upload/images/";
+      $name = date('Y') . date('m') . date("d") . date('H') . date('i') . date('s') . rand(0, 9) . '.' . $myImgSuffix;
+
+      if (is_uploaded_file($_FILES['file']['tmp_name'])) {
+
+        if (move_uploaded_file($_FILES['file']['tmp_name'], $path . $name)) {
+          echo "上传成功";
+        } else {
+          echo '上传失败';
+        }
+
+      } else {
+        echo '不是上传文件';
+      }
+
+    }
+  }
 }
--- src/routes/adminRoute.php
+++ src/routes/adminRoute.php
@@ -3,6 +3,7 @@ $app->get('/get', 'Admin:testAdmin');
 $app->post('/post', 'Admin:testAdmin');
 $app->put('/put', 'Admin:testAdmin');
 $app->delete('/delete', 'Admin:testAdmin');
+$app->post('/upload', 'Admin:upload');
 
 $app->get('/getCategory', 'Admin:getCategory');
 $app->post('/addProduct', 'Admin:addProduct');
